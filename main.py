118dbed9b833555a6e844e0bcb7c289ab9f0721d
Jaikish Pai <jkshpai@gmail.com>
2023-03-09 14:50:20 +0100

    updated code - api to return outlier score

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from fastapi import FastAPI
 import uvicorn
 from sklearn.datasets import load_iris
@@ -12,26 +13,52 @@ app = FastAPI()
 
 # Creating class to define the request body
 # and the type hints of each attribute
-class RequestBody(BaseModel):
+class request_body(BaseModel):
+    portfolio_asset_id: int
     country_encoded: float
     sub_region_name_encoded: float
-    property_type_code_encoded: float
     development_status_encoded: float
+    property_type_code_encoded: float
     asset_size_m2: float
+    en_abs: float
     en_int_kwh_m2: float
 
+
+model_name = "LOF"
+version = 1.0
 
 # setting contamination factor for outlier detection model
 contamination = 0.001
 
 model_local_outlier_factor = LocalOutlierFactor(n_neighbors=10, novelty=True, contamination=contamination)
+dataset = pd.read_csv("dataset.csv")
+model_local_outlier_factor.fit(dataset.values)
+
+
+@app.get('/info')
+async def model_info():
+    """Return model information, version, how to call"""
+    return {
+        "name": model_name,
+        "version": version
+    }
 
 
 @app.post('/outliers')
 def outliers(data: request_body):
     test_data = [[
-        data.sepal_length,
-        data.sepal_width,
-        data.petal_length,
-        data.petal_width
+        data.portfolio_asset_id,
+        data.country_encoded,
+        data.sub_region_name_encoded,
+        data.development_status_encoded,
+        data.property_type_code_encoded,
+        data.asset_size_m2,
+        data.en_abs,
+        data.en_int_kwh_m2
     ]]
+    # predict the class
+    class_predict = model_local_outlier_factor.predict(test_data)[0]
+
+    # return the result
+    return {class_predict.item()}
+
