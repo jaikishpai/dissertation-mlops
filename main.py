d88048c486f999d7c1f4665f5aead710e1b11534
Jaikish Pai <jkshpai@gmail.com>
2023-03-23 05:16:56 +0100

    updated the code to remove en_abs, and refactoring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import pandas as pd
 from fastapi import FastAPI
-import uvicorn
-from sklearn.datasets import load_iris
-from sklearn.naive_bayes import GaussianNB
 from pydantic import BaseModel
 
 # Creating FastAPI instance
@@ -10,8 +7,8 @@ from sklearn.neighbors import LocalOutlierFactor
 
 app = FastAPI(
     title="Local Outlier Factor API",
-    description="API to return outliers for Energy Intensity Data 🚀",
-    version="0.0.1",
+    description="### API for outlier detection of Energy Intensity Data 🚀 <br/>",
+    version="0.1",
     contact={
         "name": "Jaikish Pai",
         "Id": "2020SC04666"
@@ -28,18 +25,18 @@ class request_body(BaseModel):
     development_status_encoded: float
     property_type_code_encoded: float
     asset_size_m2: float
-    en_abs: float
     en_int_kwh_m2: float
 
 
-model_name = "LOF"
-version = 1.0
+model_name = "Local Outlier Factor Algorithm"
+version = 0.1
 
 # setting contamination factor for outlier detection model
 contamination = 0.001
 
 model_local_outlier_factor = LocalOutlierFactor(n_neighbors=10, novelty=True, contamination=contamination)
-dataset = pd.read_csv("dataset.csv")
+dataset = pd.read_csv("dataset .csv")
+dataset.drop("en_abs", inplace=True, axis=1)
 model_local_outlier_factor.fit(dataset.values)
 
 
@@ -61,12 +58,31 @@ def outliers(data: request_body):
         data.development_status_encoded,
         data.property_type_code_encoded,
         data.asset_size_m2,
-        data.en_abs,
         data.en_int_kwh_m2
     ]]
     # predict the class
     class_predict = model_local_outlier_factor.predict(test_data)[0]
 
-    # return the result
-    return {class_predict.item()}
+    '''
+    Returning the Outlier status
+    if the Outlier Score is -1 ===> Outlier
+    if the Outlier Score is 1 ===> Normal
+    '''
 
+    outlier = "outlier" if class_predict.item() == -1 else "normal"
+
+    '''
+    JSON Response along with Outlier status and Outlier score
+    '''
+
+    return {
+        "portfolio_asset_id": data.portfolio_asset_id,
+        "country_encoded": data.country_encoded,
+        "sub_region_name_encoded": data.sub_region_name_encoded,
+        "development_status_encoded": data.development_status_encoded,
+        "property_type_code_encoded": data.property_type_code_encoded,
+        "asset_size_m2": data.asset_size_m2,
+        "en_int_kwh_m2": data.en_int_kwh_m2,
+        "outlier_score": class_predict.item(),
+        "outlier": outlier
+    }
